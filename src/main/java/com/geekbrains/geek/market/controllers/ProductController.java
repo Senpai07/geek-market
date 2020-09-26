674799bfa1b256f87916104b52e061bcc930516f
Senpai <senpai@mail.ru>
2020-09-26 13:34:07 +0300

    Homework 5

--- src/main/java/com/geekbrains/geek/market/controllers/ProductController.java
+++ src/main/java/com/geekbrains/geek/market/controllers/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/products")
@@ -28,6 +29,23 @@ public class ProductController {
         model.addAttribute("products", products);
         model.addAttribute("filterDefinition", productFilter.getFilterDefinition());
         return "products";
+    }
+
+    @GetMapping("/edit")
+    public String editProducts(Model model, @RequestParam Long id) {
+        Optional<Product> product = productService.findById(id);
+        model.addAttribute("product", product);
+        return "edit_product";
+    }
+
+    @GetMapping("/save")
+    public String saveProducts(Model model, @RequestParam Long id, @RequestParam String title, @RequestParam int price) {
+        Product product = new Product();
+        product.setId(id);
+        product.setTitle(title);
+        product.setPrice(price);
+        productService.saveProduct(product);
+        return "redirect:/products";
     }
 
 }
