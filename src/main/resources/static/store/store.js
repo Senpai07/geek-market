eb4c3a8e77de5bab6940ed68cdcbd7cc3fab331e
Senpai <senpai@mail.ru>
2020-10-04 22:45:05 +0300

    Homework 7

--- src/main/resources/static/store/store.js
+++ src/main/resources/static/store/store.js
@@ -1,5 +1,6 @@
 angular.module('app').controller('storeController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/market';
+    var currentPage = 1;
 
     $scope.fillTable = function () {
         console.log('fill');
@@ -9,15 +10,30 @@ angular.module('app').controller('storeController', function ($scope, $http) {
             });
     };
 
-    // $scope.applyFilter = function () {
-    //     $http({
-    //         url: contextPath + '/api/v1/books',
-    //         method: "GET",
-    //         params: {obj_title: $scope.obj.title, obj_price: $scope.obj.price}
-    //     }).then(function (response) {
-    //         ...
-    //     });
-    // }
+    $scope.clearFilter = function () {
+        currentPage = 1;
+        $scope.filtrProduct = null;
+        $scope.fillTable();
+    };
+
+    $scope.applyFilter = function () {
+        currentPage = 1;
+        if ($scope.filtrProduct != null) {
+            $http({
+                url: contextPath + '/api/v1/products',
+                method: "GET",
+                params: {
+                    title: $scope.filtrProduct.title,
+                    min_price: $scope.filtrProduct.min_price,
+                    max_price: $scope.filtrProduct.max_price
+                }
+            }).then(function (response) {
+                $scope.Products = response.data;
+            });
+        } else {
+            $scope.fillTable();
+        }
+    }
 
     $scope.submitCreateNewProduct = function () {
         $http.post(contextPath + '/api/v1/products', $scope.newProduct)
@@ -27,6 +43,63 @@ angular.module('app').controller('storeController', function ($scope, $http) {
                 $scope.fillTable();
             });
     };
+
+    $scope.previous = function () {
+        if (currentPage > 1) {
+            currentPage--;
+        }
+        if ($scope.filtrProduct != null) {
+            $http({
+                url: contextPath + '/api/v1/products',
+                method: "GET",
+                params: {
+                    p: currentPage,
+                    title: $scope.filtrProduct.title,
+                    min_price: $scope.filtrProduct.min_price,
+                    max_price: $scope.filtrProduct.max_price
+                }
+            }).then(function (response) {
+                $scope.Products = response.data;
+            });
+        } else {
+            $http({
+                url: contextPath + '/api/v1/products',
+                method: "GET",
+                params: {
+                    p: currentPage
+                }
+            }).then(function (response) {
+                $scope.Products = response.data;
+            });
+        }
+    }
+    $scope.next = function () {
+        currentPage++;
+        if ($scope.filtrProduct != null) {
+            $http({
+                url: contextPath + '/api/v1/products',
+                method: "GET",
+                params: {
+                    p: currentPage,
+                    title: $scope.filtrProduct.title,
+                    min_price: $scope.filtrProduct.min_price,
+                    max_price: $scope.filtrProduct.max_price
+                }
+            }).then(function (response) {
+                $scope.Products = response.data;
+            });
+        } else {
+            $http({
+                url: contextPath + '/api/v1/products',
+                method: "GET",
+                params: {
+                    p: currentPage
+                }
+            }).then(function (response) {
+                $scope.Products = response.data;
+            });
+        }
+    }
 
     $scope.fillTable();
 });
